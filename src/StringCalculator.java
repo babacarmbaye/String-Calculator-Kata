25e53155ba32c0f56b50516d59a86956cc4a376b
mbaye.thiam <mbayediawthiam@gamil.com>
2022-06-07 12:52:22 +0200

    String calculator with one number

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -1,3 +1,13 @@
 
 public class StringCalculator {
+
+    public int add(String str) {
+
+        if (str.isEmpty()) {
+            return 0;
+        }
+
+        int result = Integer.parseInt(str);
+        return result > 0 ? result : null;
+    }
 }
--- test/StringCalculatorTest.java
+++ test/StringCalculatorTest.java
@@ -1,4 +1,6 @@
 import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
 public class StringCalculatorTest {
 
@@ -7,5 +9,12 @@ public class StringCalculatorTest {
     @Before
     public void inititalize() {
         stringCalculator = new StringCalculator();
+    }
+
+    @Test
+    public void stringCalculatorWithOneNumber() {
+        assertEquals(stringCalculator.add(""), 0);
+        assertEquals(stringCalculator.add("1"), 1);
+        assertEquals(stringCalculator.add("7"), 7);
     }
 }
